67a3965b5a55ef5357665a7c92e76bb44748be3f
sunnyddelight <sunnyddelight@gmail.com>
2015-02-14 17:54:51 -0600

    DONE NOOBS

--- processer.sv
+++ processer.sv
@@ -1,12 +1,12 @@
 module processer(input [7:0] SW, input Run, Reset, ClearA_LoadB, Clk, output[6:0] AhexU, AhexL, BhexU, BhexL, output X_val );
 	wire Add, Sub, Clr_Ld, A_out, M, Shift;
-	wire[1:0] mode;
+	wire[1:0] A_mode, B_mode;
 	wire[7:0] A_reg_val, B_reg_val, A_in;
 	wire[8:0] S, comp_A, comp_SW;
 	reg X;
 	computation_module adder(.*,.A(comp_A), .SW(comp_SW));
-	shift_reg_8 A_reg(.*, .parallel_out(A_reg_val), .parallel_load(S[7:0]), .serial_in(X),.serial_out(A_out));
-	shift_reg_8 B_reg(.*, .parallel_out(B_reg_val), .parallel_load(SW), .serial_in(A_out), .serial_out(M));
+	shift_reg_8 A_reg(.*, .mode(A_mode), .parallel_out(A_reg_val), .parallel_load(S[7:0]), .serial_in(X),.serial_out(A_out));
+	shift_reg_8 B_reg(.*, .mode(B_mode), .parallel_out(B_reg_val), .parallel_load(SW), .serial_in(A_out), .serial_out(M));
 	control control_module(.*);
 	always_ff @(posedge Clk)
 	begin
@@ -19,12 +19,22 @@ module processer(input [7:0] SW, input Run, Reset, ClearA_LoadB, Clk, output[6:0
 	assign comp_A[8]=A_reg_val[7];
 	always_comb
 	begin
-		if(Clr_Ld)
-			mode=2;
-		else if(Shift)
-			mode=1;
-		else
-			mode=0;		
+		if(Clr_Ld) begin
+			A_mode=2;
+			B_mode=2;
+		end
+		else if(Shift) begin
+			A_mode=1;
+			B_mode=1;
+		end
+		else if(Add||Sub) begin 
+			A_mode=2;
+			B_mode=0;
+		end
+		else begin
+			A_mode=0;
+			B_mode=0;
+		end
 	end
 	HexDriver A_HexUp(.In0(A_reg_val[7:4]), .Out0(AhexU));
 	HexDriver A_HexDn(.In0(A_reg_val[3:0]), .Out0(AhexL));
