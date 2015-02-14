73924a6f8db205673c7955730c3b644659210fac
jiajames <jia.james1@gmail.com>
2015-02-14 10:19:02 -0600

    added some elementary states

--- control.sv
+++ control.sv
@@ -1,1 +1,27 @@
-module control(input Run, ClearA_LoadB, Reset, output Clr_Ld, Shift, Add, Sub);
+module control(input Run, ClearA_LoadB, Reset, 
+				output Clr_Ld, Shift, Add, Sub);
+
+always_comb
+begin
+	case (state)
+		A: // Reset state
+			begin
+				Clr_Ld <= Reset;
+				Shift <= 1'b0;
+				Add <= 1'b0;
+				Sub <= 1'b0;
+			end
+		B: // ClearA_LoadB state
+			begin
+				Clr_Ld <= ClearA_LoadB;
+				Shift <= 1'b0;
+				Add <= 1'b0;
+				Sub <= 1'b0;
+		default:
+			begin
+				Clr_Ld <= 1'b0;
+				Shift <= 1'b0;
+				Add <= 1'b0;
+				Sub <= 1'b0;
+			end
+	endcase
